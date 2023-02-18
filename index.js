58e18d9476bf5483ae53872cb8ef3f8836a72c58
satyaan9951 <125714877+satyaan9951@users.noreply.github.com>
2023-02-18 08:34:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,110 @@
+let nameEl = document.getElementById("name");
+let nameErrMsgEl = document.getElementById("nameErrMsg");
+
+let emailEl = document.getElementById("email");
+let emailErrMsgEl = document.getElementById("emailErrMsg");
+
+let mobileNumberEl = document.getElementById("phone");
+let mobileNumberErrMsgEl = document.getElementById("mobileNumberErrMsg");
+
+let genderMaleEl = document.getElementById("genderMale");
+let genderFemaleEl = document.getElementById("genderFemale");
+
+let myFormEl = document.getElementById("myForm");
+
+let formData = {
+    name: "",
+    email: "",
+    phone: "",
+    gender: "Male"
+};
+
+nameEl.addEventListener("change", function(event) {
+    if (event.target.value === "") {
+        nameErrMsgEl.textContent = "Required*";
+    } else {
+        nameErrMsgEl.textContent = "";
+    }
+
+    formData.name = event.target.value;
+});
+
+emailEl.addEventListener("change", function(event) {
+    if (event.target.value === "") {
+        emailErrMsgEl.textContent = "Required*";
+    } else {
+        emailErrMsgEl.textContent = "";
+    }
+
+    formData.email = event.target.value;
+});
+
+mobileNumberEl.addEventListener("change", function(event) {
+    if (event.target.value === "" && typeof(parseInt(event.target.value)) !== 'number') {
+        mobileNumberErrMsgEl.textContent = "Required*";
+        console.log(typeof(event.target.value));
+    } else {
+        mobileNumberErrMsgEl.textContent = "";
+        console.log(typeof(parseInt(event.target.value)));
+    }
+    formData.phone = event.target.value;
+});
+
+genderMaleEl.addEventListener("change", function(event) {
+    formData.gender = event.target.value;
+});
+
+genderFemaleEl.addEventListener("change", function(event) {
+    formData.gender = event.target.value;
+});
+
+function validateFormData(formData) {
+    console.log(formData);
+    let {
+        name,
+        email,
+        phone
+    } = formData;
+    if (name === "") {
+        nameErrMsgEl.textContent = "Required*";
+    }
+    if (email === "") {
+        emailErrMsgEl.textContent = "Required*";
+    }
+    if (phone === "") {
+        mobileNumberErrMsgEl.textContent = "Required*";
+    }
+}
+
+function submitFormData(formData) {
+    let options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: "Bearer 00f3f8fde06120db02b587cc372c3d85510896e899b45774068bb750462acd9f",
+        },
+        body: JSON.stringify(formData)
+    };
+
+    let url = "https://gorest.co.in/public-api/users";
+
+    fetch(url, options)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(jsonData) {
+            console.log(jsonData);
+            if (jsonData.code === 422) {
+                if (jsonData.data[0].message === "has already been taken") {
+                    emailErrMsgEl.textContent = "Email Already Exists";
+                }
+            }
+        });
+}
+
+myFormEl.addEventListener("submit", function(event) {
+    event.preventDefault();
+    validateFormData(formData);
+    submitFormData(formData);
+});
